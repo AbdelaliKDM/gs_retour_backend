ad58d85f112afa294177eec79e1ea85433dc4e96
AbdelaliKDM <khademallah.abdelali@gmail.com>
2025-04-20 11:32:13 +0100

    fix truck creat api

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Exception;
 use App\Traits\Firebase;
-use Askedio\SoftCascade\Traits\SoftCascadeTrait;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Askedio\SoftCascade\Traits\SoftCascadeTrait;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
